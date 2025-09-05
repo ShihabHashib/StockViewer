ac5d41ed56541540923cd45ee861c3c4fd06def0
Shihab Hashib <shihabhashib1@gmail.com>
2025-09-05 14:47:50 +0600

    readme updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,14 +2,26 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from . import routes, database
 
-app = FastAPI()
+app = FastAPI(title="Stock API")
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://stockviewer-2.onrender.com",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/test")
+def test():
+    return {"message": "Backend is alive!"}
+
+@app.on_event("startup")
+def on_startup():
+    database.SQLModel.metadata.create_all(database.engine)
 
 app.include_router(routes.router)
