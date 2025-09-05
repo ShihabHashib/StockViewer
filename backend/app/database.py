0395d8902fe45cf39f2aed9651d02d4b394d1fbe
Shihab Hashib <shihabhashib1@gmail.com>
2025-09-05 15:16:20 +0600

    readme updated

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, create_engine
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("postgresql://stock_pnxc_user:cMQd1QnbyRwSrwz28sZ2yDpkEVxckfkx@dpg-d2smtap5pdvs739l9b9g-a/stock_pnxc", "sqlite:///./test.db")  # fallback to local SQLite
+DATABASE_URL = os.getenv("postgresql://stock_pnxc_user:cMQd1QnbyRwSrwz28sZ2yDpkEVxckfkx@dpg-d2smtap5pdvs739l9b9g-a.render.com:5432/stock_pnxc", "sqlite:///./test.db")  # fallback to local SQLite
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
